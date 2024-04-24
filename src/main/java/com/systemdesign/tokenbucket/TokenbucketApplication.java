0cdbdae842d39a2f2edc77b313e6a4d67ba54a07
jay-so <merry9504@naver.com>
2024-04-24 22:52:41 +0900

    Init: 프로젝트 초기 세팅

---
+++ src/main/java/com/systemdesign/tokenbucket/TokenbucketApplication.java
@@ -0,0 +1,13 @@
+package com.systemdesign.tokenbucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenbucketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenbucketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/systemdesign/tokenbucket/TokenbucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemdesign.tokenbucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenbucketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
