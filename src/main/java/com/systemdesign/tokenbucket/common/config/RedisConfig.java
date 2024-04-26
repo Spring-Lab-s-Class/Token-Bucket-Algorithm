4fa5a3a936719b7c06eacff54eb5218af1bd70c6
jay-so <merry9504@naver.com>
2024-04-27 01:52:28 +0900

    feat: Redis 설정 추가

---
+++ src/main/java/com/systemdesign/tokenbucket/common/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.systemdesign.tokenbucket.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Long> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Long> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        GenericToStringSerializer<Long> longSerializer = new GenericToStringSerializer<>(Long.class);
+        template.setValueSerializer(longSerializer);
+        template.setHashValueSerializer(longSerializer);
+
+        return template;
+    }
+}
+
+
